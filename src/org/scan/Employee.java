f2f22de4a026240182584e8ece4195fac7353786
Vickyjam <Vickyjam1996@gmail.com>
2021-11-21 12:08:11 +0530

    test

---
+++ src/org/scan/Employee.java
@@ -0,0 +1,19 @@
+package org.scan;
+
+import java.util.Scanner;
+
+public class Employee {
+	public static void main(String[] args) {
+	Scanner s= new Scanner(System.in);
+	
+	
+	String next = s.next();
+	System.out.println(next);
+	
+	String nextLine = s.nextLine();
+	System.out.println(nextLine);
+	
+	
+	}
+
+}
